ccb99f96f99d2ddbf0b48b6a4106ef26f88dfff6
David Spector <spector@zeitgeist.com>
2015-11-11 10:41:47 -0800

    Added tvOS framework target

---
+++ SWXMLHashtvOS/SWXMLHashtvOS.h
@@ -0,0 +1,19 @@
+//
+//  SWXMLHashtvOS.h
+//  SWXMLHashtvOS
+//
+//  Created by David HM Spector on 11/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWXMLHashtvOS.
+FOUNDATION_EXPORT double SWXMLHashtvOSVersionNumber;
+
+//! Project version string for SWXMLHashtvOS.
+FOUNDATION_EXPORT const unsigned char SWXMLHashtvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWXMLHashtvOS/PublicHeader.h>
+
+
---
+++ SWXMLHashtvOSTests/SWXMLHashtvOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  SWXMLHashtvOSTests.swift
+//  SWXMLHashtvOSTests
+//
+//  Created by David HM Spector on 11/11/15.
+//
+//
+
+import XCTest
+@testable import SWXMLHashtvOS
+
+class SWXMLHashtvOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
